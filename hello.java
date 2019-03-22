85c602ba4ec63f01f01d0003f4a8dcea2155fb40
lqm <lqmingy@163.com>
2019-03-22 18:12:40 +0800

    第一次修改hello.java文件

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		
-		System.out.println("我的世界....");
+		System.out.println("我的世界启动中...");
+		System.out.println("第一次启动创建地图中...");
 	}
 	
 }
